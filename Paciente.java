bbfd158483ec43e7be8be501dc5220d1c3336ed9
DijanU <156262584+DijanU@users.noreply.github.com>
2024-04-07 18:06:30 -0600

    clase paciente
    clase paciente

---
+++ Paciente.java
@@ -0,0 +1,25 @@
+public class Paciente {
+    private String name;
+    private String condition;
+    private String priority;
+
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getCondition() {
+        return condition;
+    }
+    public void setCondition(String condition) {
+        this.condition = condition;
+    }
+    public String getPriority() {
+        return priority;
+    }
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+}
